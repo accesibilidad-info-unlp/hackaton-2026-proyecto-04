4dbc2e94e3c28218428f1c57fc033ac8ebaf8568
buschiazzoMarco2 <marcobuschiazzo@gmail.com>
2026-06-06 14:55:38 -0300

    funcionalidad

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -2,12 +2,18 @@ const paradasFav = document.getElementById("paradas-fav");
 const paradasCercanas = document.getElementById("paradas-cercanas");
 const lineas = document.getElementById("lineas");
 const recorridos = document.getElementById("recorridos");
-const opcionesUno = document.getElementById("opcionesUno");
-const opcionesLinea = document.getElementById("opcioneslineas");
-const opcionesParada = document.getElementById("opcionesParadas");
-
-paradasFav.addEventListener("click", () => {
-  for (let i = 0; i < document.getElementsByClassName("buttons").length; i++) {
+//const opcionesUno = document.getElementById("opcionesUno");
+//const opcionesLinea = document.getElementById("Linea");
+//const opcionesParada = document.getElementById("opcionesParadas");
+function esconder(){
+    for (let i = 0; i < document.getElementsByClassName("buttons").length; i++) {
     document.getElementsByClassName("buttons")[i].style.display = "none";
   }
-});
+}
+function cambiar(valor){
+    esconder()
+    document.getElementById(valor).style.display="flex"
+}
+lineas.addEventListener("click", (e) => {cambiar(e.target.value)});
+paradasFav.addEventListener("click", (e) => {cambiar(e.target.value)});
+paradasCercanas.addEventListener("click", (e) => {cambiar(e.target.value)});
