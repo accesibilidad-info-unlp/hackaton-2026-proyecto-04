9b244f268ee2368115ade0538ef81b5d5a9958a2
tomgalha <tomgalha@gmail.com>
2026-06-06 12:28:03 -0300

    Acceder a localizacion del usuario

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-const map = L.map('map').setView([-34.9214,-57.9544],13);
+function AccederLocalizacion() {
+  navigator.geolocation.getCurrentPosition(
+    function (posicion) {
+      console.log("Gracias por el permiso. Tu latitud es: " + posicion.coords.latitude);
+    },
+    function (error) {
+      if (error.code === 1) {
+        console.log("El usuario rechazó el cartel nativo del navegador.");
+      }
+    }
+  );
+}
+
+AccederLocalizacion();
