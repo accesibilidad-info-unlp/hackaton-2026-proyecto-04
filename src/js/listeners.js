4c6ebf663b4a6efe6100328eac13326338806a72
buschiazzoMarco2 <marcobuschiazzo@gmail.com>
2026-06-06 14:33:36 -0300

    .

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -2,6 +2,9 @@ const paradasFav = document.getElementById("paradas-fav");
 const paradasCercanas = document.getElementById("paradas-cercanas");
 const lineas = document.getElementById("lineas");
 const recorridos = document.getElementById("recorridos");
+const opcionesUno = document.getElementById("opcionesUno");
+const opcionesLinea = document.getElementById("opcioneslineas");
+const opcionesParada = document.getElementById("opcionesParadas");
 
 paradasFav.addEventListener("click", () => {
   for (let i = 0; i < document.getElementsByClassName("buttons").length; i++) {
