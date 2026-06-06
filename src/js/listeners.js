dc39493230118da36d6e77145b9a9e5bdbf78814
tomgalha <tomgalha@gmail.com>
2026-06-06 14:12:52 -0300

    agregale id a los botones y agregar los listeners

---
+++ src/js/listeners.js
@@ -0,0 +1,6 @@
+const paradasFav = document.getElementById("paradas-fav");
+const paradasCercanas = document.getElementById("paradas-cercanas");
+const lineas = document.getElementById("lineas");
+const recorridos = document.getElementById("recorridos");
+
+paradasFav.addEventListener("click", () => console.log("Click en paradas fav"));
