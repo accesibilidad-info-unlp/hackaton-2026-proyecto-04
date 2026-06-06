a82473112218ef835cee9a4f454c37b5afcc9eb8
tomgalha <tomgalha@gmail.com>
2026-06-06 13:59:57 -0300

    Borrar doble footer y  meter las paradas de micro dentro de un array

--- src/main.js
+++ src/main.js
@@ -1,62 +1,65 @@
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const stops = [
+  {
+    name: "rocha",
+    ubicacion: "-34.92087071516999, -57.94155961864684",
+    micros: ["mUno", "mTres"],
+  },
+  {
+    name: "italia",
+    ubicacion: "-34.91077864824624, -57.9552758912345",
+    micros: ["mUno", "mDos"],
+  },
+  {
+    name: "azcuenaga",
+    ubicacion: "-34.92205274298061, -57.967536701181764",
+    micros: ["mUno", "mDos"],
+  },
+  {
+    name: "yrigoyen",
+    ubicacion: "-34.931939435623384, -57.95423611121962",
+    micros: ["mUno", "mTres"],
+  },
+];
 
 function savePosition() {
   const exito = (position) => {
     const ubicacion = {
       lat: position.coords.latitude,
       lon: position.coords.longitude,
-      timestamp: Date.now()
+      timestamp: Date.now(),
     };
-    localStorage.setItem('user_location', JSON.stringify(ubicacion));
-    console.log('Localización guardada en el localStorage');
+    localStorage.setItem("user_location", JSON.stringify(ubicacion));
+    console.log("Localización guardada en el localStorage");
   };
 
   const error = (errorObjeto) => {
     if (errorObjeto.code === errorObjeto.PERMISSION_DENIED) {
-      console.warn('El usuario tocó que NO / Bloqueó el acceso.');
-      alert('Necesitamos tu ubicación para mostrar el mapa. Por favor, actívala en tu navegador.');
+      console.warn("El usuario tocó que NO / Bloqueó el acceso.");
+      alert(
+        "Necesitamos tu ubicación para mostrar el mapa. Por favor, actívala en tu navegador.",
+      );
     } else {
-      console.error('Ocurrió otro error:', errorObjeto.message);
+      console.error("Ocurrió otro error:", errorObjeto.message);
     }
   };
 
   navigator.geolocation.getCurrentPosition(exito, error);
 }
 const getPosition = () => {
-  const position = localStorage.getItem('user_location');
+  const position = localStorage.getItem("user_location");
   console.log(position);
-}
+};
 
 function inicializarMapa() {
-  const map = L.map('map').setView([-34.9214, -57.9544], 20);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
-  }).addTo(map); // ← esto falta
+  const map = L.map("map").setView([-34.9214, -57.9544], 15);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap",
+  }).addTo(map);
 }
-
 
 savePosition();
 getPosition();
 inicializarMapa();
-
-const stopOne ={
-    name: "rocha",
-    ubicacion: "-34.92087071516999, -57.94155961864684",
-    micros: ["mUno","mTres"]
-}
-const stopTwo ={
-    name: "italia",
-    ubicacion: "-34.91077864824624, -57.9552758912345",
-    micros: ["mUno","mDos"]
-}
-const stopThree ={
-    name: "azcuenaga",
-    ubicacion: "-34.92205274298061, -57.967536701181764",
-    micros: ["mUno","mDos"]
-}
-const stopFour ={
-    name: "yrigoyen",
-    ubicacion: "-34.931939435623384, -57.95423611121962",
-    micros: ["mUno","mTres"]
-}
