64f881ea78f3b32ae50f5b3cd279a35e529cd63f
tomgalha <tomgalha@gmail.com>
2026-06-06 13:23:58 -0300

    Agregar function para invocar el mapa

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-const map = L.map('map').setView([-34.9214,-57.9544],13);
+import L from 'leaflet'
+/*const stopOne = {
+  name: "parada 1",
+  ubicacion: "-34.92087071516999, -57.94155961864684",
+  micros: []
+}*/
+
 function savePosition() {
   const exito = (position) => {
     const ubicacion = {
@@ -22,16 +28,21 @@ function savePosition() {
   navigator.geolocation.getCurrentPosition(exito, error);
 }
 const getPosition = () => {
+
   const position = localStorage.getItem('user_location');
   console.log(position);
 }
 
+
+function inicializarMapa() {
+  const map = L.map('map').setView([-34.9214, -57.9544], 20);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+  }).addTo(map); // ← esto falta
+}
+
+
 savePosition();
 getPosition();
-AccederLocalizacion();
+inicializarMapa();
 
-const stopOne{
-    name="parada 1",
-    ubicacion="-34.92087071516999, -57.94155961864684",
-    micros=
-}
