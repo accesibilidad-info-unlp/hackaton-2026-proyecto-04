d7cf79e10c557c65a3926c2218b7341725e663c0
Joaquín Segura <joaquinsegura.dev@gmail.com>
2026-06-06 14:31:52 -0300

    function esconder

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -3,4 +3,8 @@ const paradasCercanas = document.getElementById("paradas-cercanas");
 const lineas = document.getElementById("lineas");
 const recorridos = document.getElementById("recorridos");
 
-paradasFav.addEventListener("click", () => console.log("Click en paradas fav"));
+paradasFav.addEventListener("click", () => {
+  for (let i = 0; i < document.getElementsByClassName("buttons").length; i++) {
+    document.getElementsByClassName("buttons")[i].style.display = "none";
+  }
+});
