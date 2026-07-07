ec0e294ca4f539d00554bd42847f048fbb492f13
tomgalha <tomgalha@gmail.com>
2026-07-07 19:30:57 -0300

    mapa minimalista

--- src/js/main.js
+++ src/js/main.js
@@ -52,8 +52,10 @@ class Map {
 
   construirMapa() {
     this._map = L.map("map").setView([this.lat, this.long], 16);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap",
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20
     }).addTo(this._map);
 
     L.circleMarker([this._lat, this._long], {
