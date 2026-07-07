1fed4627a5941f357e9faa995a5c56d2b80da86a
jqw <joaquinsegura.dev@gmail.com>
2026-07-07 18:15:15 -0300

    cambio de fuente de mapas

--- src/js/main.js
+++ src/js/main.js
@@ -55,8 +55,10 @@ const getPosition = () => {
 
 function inicializarMapa() {
   const map = L.map("map").setView([-34.9214, -57.9544], 15);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "© OpenStreetMap",
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
   }).addTo(map);
 }
 
